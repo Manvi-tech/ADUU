9d70e0aeda33f802265c4458b48c90e9ffecda16
Manvi Bansal <bansalmanvi1409@gmail.com>
2021-11-26 16:56:05 +0530

    dev file edited

---
+++ config/dev.js
@@ -0,0 +1,16 @@
+
+
+module.exports = {
+    google:{
+        clientID:'416241789475-nbijsvfoomhsv1oenif4hv8bl867mha8.apps.googleusercontent.com',
+        clientSecret:'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'
+    },
+    session:{
+        name: 'timetorelive',
+        cookieKey: 'PleaseBeKindToAnimals'
+    },
+    mongodb:{
+        url: 'mongodb+srv://manvi:ivnam@cluster0.kruqg.mongodb.net/EduLive?retryWrites=true&w=majority'
+    }
+
+}
